691ff6435694a83084d5a05e1d4a6dd17f71651b
Rody Castillo <rody.castillo.tacuri@gmail.com>
2020-09-23 18:10:28 -0500

    Update title page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StarwarsModule } from './modules/starwars/starwars.module';
-import { StarwarsComponent } from './modules/starwars/starwars.component';
 
 @NgModule({
   declarations: [AppComponent],
